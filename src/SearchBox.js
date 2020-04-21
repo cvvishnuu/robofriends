227f4b2ff352836696ff6c218c74de4b52f124b0
cvvishnuu <cvishnuu01@gmail.com>
2020-04-21 21:33:49 +0530

    untracked files commited

---
+++ src/SearchBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SearchBox = ({ searchfield, searchchange }) => {
+	return (
+		<div className = 'pa2'>
+			<input 
+				className = 'pa3 ba b--green bg-lightest-blue' 
+				type = 'search' cxx
+				placeholder = 'Search Robots' 
+				onChange = {searchchange}
+			/>
+		</div>
+	)
+}
+
+export default SearchBox;     
---
+++ src/robots.js
@@ -0,0 +1,78 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'C.V.Vishnuu',
+		username: 'Vishnuu',
+		email: 'cvishnuu01@gmail.com',
+	},
+
+	{
+		id: 2,
+		name: 'R.Indra Mohini',
+		username: 'Indra',
+		email: 'indra@gmail.com',
+	},
+
+	{
+		id: 3,
+		name: 'C.K.Venu Kuttan',
+		username: 'Venu',
+		email: 'venu@gmail.com',
+	},
+
+	{
+		id: 4,
+		name: 'Jenisha Gopinath',
+		username: 'Jenisha',
+		email: 'jenishagopinath@gmail.com',
+	},
+
+	{
+		id: 5,
+		name: 'Ajay Maheswaran',
+		username: 'Ajay',
+		email: 'ajay@gmail.com',
+	},
+
+	{
+		id: 6,
+		name: 'Dharmish',
+		username: 'Dharmish',
+		email: 'dharmish@gmail.com',
+	},
+
+	{
+		id: 7,
+		name: 'Mark Joseph',
+		username: 'Mark',
+		email: 'Markj@gmail.com',
+	},
+
+	{
+		id: 8,
+		name: 'Nithin Mishra',
+		username: 'Nithin',
+		email: 'nithin@gmail.com',
+	},
+
+	{
+		id: 9,
+		name: 'Arshath Raj',
+		username: 'Arshath',
+		email: 'arshath@gmail.com',
+	},
+
+	{
+		id: 10,
+		name: 'Neeraj Sunil',
+		username: 'Neeraj',
+		email: 'neeraj@gmail.com',
+	},
+
+	{
+		id: 11,
+		name: 'Sofia',
+		username: "Sofi",
+		email: 'sofia@gmail.com',
+	}
+];
