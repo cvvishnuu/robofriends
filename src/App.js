a473fbeff5c9199e2e299da66e1fd22402dd6a71
cvvishnuu <cvishnuu01@gmail.com>
2020-04-22 00:56:52 +0530

    commiting all the changes affter styling the project background and heading

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
+import './App.css';
 
 class App extends Component {
 	constructor(){
@@ -21,7 +22,7 @@ class App extends Component {
 		});
 		return(
 			<div className = 'tc'>
-				<h1>Robot Friends</h1>
+				<h1 className = 'f1'>Robot Friends</h1>
 				<SearchBox searchchange = {this.onSearchChange}/>
 				<CardList robots = {filteredRobots } />
 			</div>
