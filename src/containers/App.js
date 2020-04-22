6bc3247f50d5c5fd993a751aaadfeaa0f0beeb4e
cvvishnuu <cvishnuu01@gmail.com>
2020-04-22 16:12:00 +0530

    Folder components and containers created

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import { robots } from './robots';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import { robots } from '../robots';
 import './App.css';
-import Scroll from './Scroll';
+import Scroll from '../components/Scroll';
 
 class App extends Component {
 	constructor(){
